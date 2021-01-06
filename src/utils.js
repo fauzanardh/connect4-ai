1343d77a6b61ec3c4138f84ec223c524a36a2dde
fauzanardh <31564867+fauzanardh@users.noreply.github.com>
2021-01-06 19:22:54 +0700

    move out off dist folder

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    range: num => [...Array(num).keys()],
-    key: (row, col) => `${row}${col}`,
-    cssUrl: id => `url(#${id})`,
-    makeTitle: text => text[0].toUpperCase() + text.slice(1, text.length),
-    min: num => Math.max(num - 3, 0),
-    max: (num, max) => Math.min(num + 3, max)
-}
