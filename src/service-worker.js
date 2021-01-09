bb2eddb40cd8d50c21a9feb7c43af6d99ab43e78
fauzanardh <31564867+fauzanardh@users.noreply.github.com>
2021-01-09 23:07:51 +0700

    rollback

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,12 +3,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 if (workbox) {
     workbox.core.setCacheNameDetails({prefix: "connect4-ai"});
 
-    self.addEventListener('message', (event) => {
-        if (event.data && event.data.type === 'SKIP_WAITING') {
-            self.skipWaiting();
-        }
-    });
-
+    workbox.core.skipWaiting();
     workbox.core.clientsClaim();
     workbox.precaching.cleanupOutdatedCaches();
     workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
