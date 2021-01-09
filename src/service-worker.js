87e7ec47c66098ff091c1051a47493494c4f4cd6
fauzanardh <31564867+fauzanardh@users.noreply.github.com>
2021-01-09 23:01:50 +0700

    Add checks on service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,15 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js')
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 if (workbox) {
-    workbox.core.skipWaiting();
+    workbox.core.setCacheNameDetails({prefix: "connect4-ai"});
+
+    self.addEventListener('message', (event) => {
+        if (event.data && event.data.type === 'SKIP_WAITING') {
+            self.skipWaiting();
+        }
+    });
+
     workbox.core.clientsClaim();
     workbox.precaching.cleanupOutdatedCaches();
-
     workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 }
